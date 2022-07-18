0787670d2cfe3ebcf60f6ae06b44ce14b55e2f52
1kwl <54815293+1kwl@users.noreply.github.com>
2022-07-18 21:14:03 +0300

    Add files via upload

---
+++ kovalkov_ilya_dmitrievich/task_1_1.py
@@ -0,0 +1,10 @@
+a = 5
+b = 5
+
+summ = a + b
+difference = a - b
+multiplication = a * b
+
+print('сумма чисел = ' + str(summ))
+print('разность чисел = ' + str(difference))
+print('произведение чисел = ' + str(multiplication))
